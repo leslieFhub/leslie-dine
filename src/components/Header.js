2f56fbd5d85b46aaa99a714eb60b61dda000acdd
leslieFhub <134671364+leslieFhub@users.noreply.github.com>
2023-06-04 15:01:56 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,207 +1,48 @@
-import React, { useState } from "react";
-import { Link, useHistory } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { logout } from "../Redux/Actions/userActions";
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
-  const [keyword, setKeyword] = useState();
-  const dispatch = useDispatch();
-  let history = useHistory();
-
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
-  const userLogin = useSelector((state) => state.userLogin);
-  const { userInfo } = userLogin;
 
-  const logoutHandler = () => {
-    dispatch(logout());
-  };
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    if (keyword.trim()) {
-      history.push(`/search/${keyword}`);
-    } else {
-      history.push("/");
-    }
-  };
-
-  const refreshPage = () => {
+  const handleRefreshClick = () => {
     window.location.reload();
   };
 
   return (
     <div>
       {/* Top Header */}
-      <div className="Announcement ">
+      <div className="Announcement">
         <div className="container">
-          <div className="row">
-            <div className="col-md-6 d-flex align-items-center display-none">
-              <Link to="/about">
-                <p>ABOUT</p>
-              </Link>
-              <Link to="/faq">
-                <p>FAQs</p>
+          <div className="row justify-content-center align-items-center">
+            <div className="col-md-6 d-flex align-items-center justify-content-center justify-content-md-start">
+              <Link to="/" 
+                 onClick={() => {
+                  window.location.href = "/";
+                }}
+                  style={{ color: "black" }}
+                  >
+                <div className="d-flex align-items-center">
+                  <span>Leslies Pizza </span>
+                  <span style={{ color: "yellow" }}> Foodhub</span>
+                </div>
               </Link>
             </div>
-            <div className=" col-12 col-lg-6 justify-content-center justify-content-lg-end d-flex align-items-center">
-              <Link to="">
-                <i className="fab fa-facebook-f"></i>
+            <div className="col-12 col-lg-6 d-flex justify-content-center justify-content-lg-end align-items-center">
+              <Link to="/cart">
+                <i className="fas fa-shopping-bag"></i>
+                <span className="badge">{cartItems.length}</span>
               </Link>
+              <button className="btn btn-link" onClick={handleRefreshClick}>
+                <i className="fas fa-sync-alt"></i>
+              </button>
             </div>
           </div>
         </div>
       </div>
-      {/* Header */}
-      <div className="header">
-        <div className="container">
-          {/* MOBILE HEADER */}
-          <div className="mobile-header">
-            <div className="container ">
-              <div className="row ">
-                <div className="col-6 d-flex align-items-center">
-                  <Link className="navbar-brand" to="/" onClick={refreshPage}>
-                    <img alt="logo" src="/images/LESLIES_PIZZA_FOODHUB.png" />
-                  </Link>
-                </div>
-                <div className="col-6 d-flex align-items-center justify-content-end Login-Register">
-                  {userInfo ? (
-                    <div className="btn-group">
-                      <button
-                        type="button"
-                        className="name-button dropdown-toggle"
-                        data-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false"
-                      >
-                        <i className="fas fa-user"></i>
-                      </button>
-                      <div className="dropdown-menu">
-                        <Link className="dropdown-item" to="/profile">
-                          Profile
-                        </Link>
 
-                        <Link
-                          className="dropdown-item"
-                          to="#"
-                          onClick={logoutHandler}
-                        >
-                          Logout
-                        </Link>
-                      </div>
-                    </div>
-                  ) : (
-                    <div className="btn-group">
-                      <button
-                        type="button"
-                        className="name-button dropdown-toggle"
-                        data-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false"
-                      >
-                        <i className="fas fa-user"></i>
-                      </button>
-                      <div className="dropdown-menu">
-                        <Link className="dropdown-item" to="/login">
-                          Login
-                        </Link>
-
-                        <Link className="dropdown-item" to="/register">
-                          Register
-                        </Link>
-                      </div>
-                    </div>
-                  )}
-
-                  <Link to="/cart" className="cart-mobile-icon">
-                    <i className="fas fa-shopping-bag"></i>
-                    <span className="badge">{cartItems.length}</span>
-                  </Link>
-                </div>
-                <div className="col-12 d-flex align-items-center">
-                  <form onSubmit={submitHandler} className="input-group">
-                    <input
-                      type="search"
-                      className="form-control rounded search"
-                      placeholder="Search"
-                      onChange={(e) => setKeyword(e.target.value)}
-                    />
-                    <button type="submit" className="search-button">
-                      search
-                    </button>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* PC HEADER */}
-          <div className="pc-header">
-            <div className="row">
-              <div className="col-md-3 col-4 d-flex align-items-center">
-                <Link
-                  className="navbar-brand"
-                  to="/"
-                  onClick={refreshPage}
-                >
-                  <img alt="logo" src="/images/LESLIES_PIZZA_FOODHUB.png" />
-                </Link>
-              </div>
-              <div className="col-md-6 col-8 d-flex align-items-center">
-                <form onSubmit={submitHandler} className="input-group">
-                  <input
-                    type="search"
-                    className="form-control rounded search"
-                    placeholder="Search"
-                    onChange={(e) => setKeyword(e.target.value)}
-                  />
-                  <button type="submit" className="search-button">
-                    search
-                  </button>
-                </form>
-              </div>
-              <div className="col-md-3 d-flex align-items-center justify-content-end Login-Register">
-                {userInfo ? (
-                  <div className="btn-group">
-                    <button
-                      type="button"
-                      className="name-button dropdown-toggle"
-                      data-toggle="dropdown"
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                    >
-                      Hi, {userInfo.name}
-                    </button>
-                    <div className="dropdown-menu">
-                      <Link className="dropdown-item" to="/profile">
-                        Profile
-                      </Link>
-
-                      <Link
-                        className="dropdown-item"
-                        to="#"
-                        onClick={logoutHandler}
-                      >
-                        Logout
-                      </Link>
-                    </div>
-                  </div>
-                ) : (
-                  <>
-                    <Link to="/register">Register</Link>
-                    <Link to="/login">Login</Link>
-                  </>
-                )}
-
-                <Link to="/cart">
-                  <i className="fas fa-shopping-bag"></i>
-                  <span className="badge">{cartItems.length}</span>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      
     </div>
   );
 };
